2d7d60a665d33ef091df5e1641d3c33bdd34a6b2
Maxim <yourstenex@gmail.com>
2023-04-23 19:35:53 +0300

    init form sumbit handlers for auth pages

--- frontend/src/components/auth/LoginForm.tsx
+++ frontend/src/components/auth/LoginForm.tsx
@@ -1,17 +1,48 @@
 import { Link } from "react-router-dom";
 import { AuthFormWrapper } from "./FormWrapper";
 import { TextField } from "@/uikit/inputs";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object({
+  email: Yup.string().email("Email should be valid").required("Email is required"),
+  password: Yup.string().required("Password is required"),
+});
+
+type FormValues = {
+  email: string;
+  password: string;
+};
 
 export const LoginForm = () => {
+  const formik = useFormik<FormValues>({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema,
+    onSubmit: async (values) => {
+      console.log(values);
+    },
+    validateOnBlur: true,
+    validateOnChange: false,
+  });
+
   return (
     <AuthFormWrapper
       title="Log in"
       content={
-        <form>
-          <TextField label="Email" type="email" />
-          <TextField label="Password" type="password" />
+        <form onSubmit={formik.handleSubmit}>
+          <TextField {...formik.getFieldProps("email")} label="Email" type="email" />
+          <TextField
+            {...formik.getFieldProps("password")}
+            label="Password"
+            type="password"
+          />
           <div className="flex mt-3">
-            <button className="primary-btn mr-3 flex-[1_0_48%]">Login</button>
+            <button type="submit" className="primary-btn mr-3 flex-[1_0_48%]">
+              Login
+            </button>
             <Link to="/register" className="secondary-btn w-48 !px-0">
               Register an account
             </Link>
--- frontend/src/components/auth/RegisterForm.tsx
+++ frontend/src/components/auth/RegisterForm.tsx
@@ -1,32 +1,89 @@
 import { Link } from "react-router-dom";
 import { AuthFormWrapper } from "./FormWrapper";
 import { TextField } from "@/uikit/inputs";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object({
+  email: Yup.string().email("Email should be valid").required("Email is required"),
+  firstName: Yup.string()
+    .max(100, "Maximum length is 50")
+    .required("First name is required"),
+  lastName: Yup.string()
+    .max(100, "Maximum length is 50")
+    .required("Last name should be required"),
+  password: Yup.string()
+    .min(7, "Minimum length is 7")
+    .max(255, "Maximum length is 255")
+    .required("Password is required"),
+  confirmPassword: Yup.string().test(
+    "Passwords match",
+    "Passwords must match",
+    function (value) {
+      return this.parent.password === value;
+    }
+  ),
+});
+
+type FormValues = {
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  confirmPassword: string;
+};
 
 export const RegisterForm = () => {
+  const formik = useFormik<FormValues>({
+    initialValues: {
+      email: "",
+      firstName: "",
+      lastName: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validationSchema,
+    onSubmit: async (values) => {
+      console.log(values);
+    },
+    validateOnBlur: true,
+    validateOnChange: false,
+  });
+
   return (
     <AuthFormWrapper
       title="Register form"
       content={
-        <form className="flex flex-col">
-          <TextField label="Email" type="email" />
+        <form className="flex flex-col" onSubmit={formik.handleSubmit}>
+          <TextField {...formik.getFieldProps("email")} label="Email" type="email" />
           <div className="flex justify-between">
             <div className="flex-1 max-w-[48%]">
-              <TextField label="First Name" />
+              <TextField {...formik.getFieldProps("firstName")} label="First Name" />
             </div>
             <div className="flex-1 max-w-[48%]">
-              <TextField label="Last Name" />
+              <TextField {...formik.getFieldProps("lastName")} label="Last Name" />
             </div>
           </div>
           <div className="flex justify-between">
             <div className="flex-1 max-w-[48%]">
-              <TextField label="Password" type="password" />
+              <TextField
+                {...formik.getFieldProps("password")}
+                label="Password"
+                type="password"
+              />
             </div>
             <div className="flex-1 max-w-[48%]">
-              <TextField label="Confirm password" type="password" />
+              <TextField
+                {...formik.getFieldProps("confirmPassword")}
+                label="Confirm password"
+                type="password"
+              />
             </div>
           </div>
           <div className="mt-3 flex">
-            <button className="primary-btn mr-3 max-w-[48%] flex-1">Register</button>
+            <button type="submit" className="primary-btn mr-3 max-w-[48%] flex-1">
+              Register
+            </button>
             <Link to="/login" className="secondary-btn flex-1 !px-0 max-w-[48%]">
               Have an account?
             </Link>
