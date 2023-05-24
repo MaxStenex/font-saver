b11ce9db777b7d76057267992e9805bd309396a9
Maxim <yourstenex@gmail.com>
2023-05-24 22:03:06 +0300

    vitest setup, added tests for classnames util

---
+++ frontend/src/utils/classNames.test.ts
@@ -0,0 +1,42 @@
+import { describe, expect, it } from "vitest";
+import { classNames } from "./classNames";
+
+describe("ClassNames test", () => {
+  it("If string passed as argument, should return it", () => {
+    const argument = "foo";
+    const result = classNames(argument);
+
+    expect(result).toBe(argument);
+  });
+
+  it("If more than one strings passed as arguments, should return them as concatinated string, with divider", () => {
+    const firstClass = "foo";
+    const secondClass = "bar";
+    const result = classNames(firstClass, secondClass);
+
+    expect(result).toBe(`${firstClass} ${secondClass}`);
+  });
+
+  it("Should correctly parse classes from object argument", () => {
+    const argument1 = "foo";
+    const argument2 = { bar: true, fooBar: false };
+    const result = classNames(argument1, argument2);
+
+    expect(result).toBe(`${argument1} bar`);
+  });
+
+  it("Should return empy string, if all classes are inactive", () => {
+    const argument = { "foo-bar": false, foo: false, bar: false };
+    const result = classNames(argument);
+
+    expect(result).toBe("");
+  });
+
+  it("Should correctly parse multiple objects", () => {
+    const argument1 = { "foo-bar": true, foo: false };
+    const argument2 = { bar: true };
+    const result = classNames(argument1, argument2);
+
+    expect(result).toBe(`foo-bar bar`);
+  });
+});
