3d82d3113592307518fd7890ad6dbab4d9657185
Maxim <yourstenex@gmail.com>
2023-06-19 23:52:45 +0300

    eslintrc setup fixes, removed any casts

--- backend/src/filters/query-error.filter.ts
+++ backend/src/filters/query-error.filter.ts
@@ -2,9 +2,14 @@ import { ArgumentsHost, BadRequestException, Catch } from "@nestjs/common";
 import { BaseExceptionFilter } from "@nestjs/core";
 import { QueryFailedError } from "typeorm";
 
+interface Exception extends QueryFailedError {
+  detail: string;
+  table: string;
+}
+
 @Catch(QueryFailedError)
 export class QueryErrorFilter extends BaseExceptionFilter {
-  public catch(exception: any, host: ArgumentsHost): any {
+  public catch(exception: Exception, host: ArgumentsHost): void {
     const detail = exception.detail;
 
     if (typeof detail === "string" && detail.includes("already exists")) {
--- backend/src/migration/1687111876519-UniqueUserFields.ts
+++ backend/src/migration/1687111876519-UniqueUserFields.ts
@@ -1,16 +1,23 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class UniqueUserFields1687111876519 implements MigrationInterface {
-    name = 'UniqueUserFields1687111876519'
+  name = "UniqueUserFields1687111876519";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email")`);
-        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "UQ_78a916df40e02a9deb1c4b75edb" UNIQUE ("username")`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email")`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD CONSTRAINT "UQ_78a916df40e02a9deb1c4b75edb" UNIQUE ("username")`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "UQ_78a916df40e02a9deb1c4b75edb"`);
-        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" DROP CONSTRAINT "UQ_78a916df40e02a9deb1c4b75edb"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" DROP CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22"`,
+    );
+  }
 }
